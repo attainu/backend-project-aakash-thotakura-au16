18bc08da988456132c2b32f89b6b04763b846d70
aakash emmanuel <aakash.1323@gmail.com>
2021-05-24 23:39:46 +0530

    Created paths, installed needed packages

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const expHbs = require('express-handlebars')
+// const cookieParser = require('cookie-parser')
+const session = require('express-session')
+
+app.engine('hbs', expHbs({ extname: 'hbs'  }))
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+app.use(express.json());
+
+
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+	res.render('ds-main')
+})
+
+app.post('/signIn', (req, res) => {
+	console.log('text')
+	// res.json({ details: 'enter your details to signIn' })
+	
+})
+
+app.get('/signUp', (req, res) => {
+	console.log('rendering signUp')
+	res.render('signUp')
+})
+
+app.post('/notepad', (req, res) => {
+	res.send('this is a notepad for your to scribble')
+})
+
+app.post('/events', (req, res) => {
+	res.send('add important events')
+})
+
+app.post('/tasks', (req, res) => {
+	res.send('add list of to-do lists with reminder')
+})
+
+app.post('/photos', (req, res) => {
+	res.send('save your pictures')
+})
+
+app.post('/videos', (req, res) => {
+	res.send('save your videos')
+})
+
+app.post('/game', (req, res) => {
+	res.send('idea is not developed yet')
+})
+
+app.post('/record', (req, res) => {
+	res.send('record songs or notes')
+})
+
+app.post('/books', (req, res) => {
+	res.send('track your books (fav, finished, to_be_read)')
+})
+
+app.listen(3000, () => {console.log('Server Initiated')})
