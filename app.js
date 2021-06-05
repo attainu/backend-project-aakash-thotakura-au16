952ebef35c04a8cdc66bb3a811550c60a6a87f39
aakash emmanuel <aakash.1323@gmail.com>
2021-06-05 23:51:23 +0530

    confirmed changes

--- app.js
+++ app.js
@@ -1,84 +1,84 @@
-//const {initDatabaseConnection} = require('./config/database')
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const fs = require('fs/promises');
 const expHbs = require('express-handlebars');
 const session = require('express-session');
 const router = require('./routes/index');
-const cookieParser = require('cookie-parser');
 const expressUpload = require('express-fileupload');
 const cloudinary = require('cloudinary').v2
-
+// const jwt = require('jsonwebtoken');
+const base64 = require('base64-arraybuffer')
 
 
 app.engine('hbs', expHbs({ extname: 'hbs'  }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
-
 app.use(express.json());
-app.use(cookieParser())
+app.use(expressUpload());
 app.use(express.urlencoded({ extended: true }))
 
 
 app.use('/', router);
 
-app.get('/index', async (req, res) => {
-	res.render('index')
+app.get('/', async (req, res) => {
+	res.send('index')
 })
 
-cloudinary.config({ 
-    cloud_name: 'beproject', 
-    api_key: process.env.API_KEY, 
-    api_secret: process.env.API_SECRET
-  });
+// cloudinary.config({ 
+//     cloud_name: 'beproject',
+//     api_key: process.env.API_KEY,
+//     api_secret: process.env.API_SECRET
+// });
 
 
-app.get('/test', (req, res) => {
-    res.render('test')
-})
+// app.get('/test', (req, res) => {
+//     res.render('test')
+// })
 
-app.post('/test', async (req, res) => {
+// app.post('/test', async (req, res) => {
 
-    console.log(req.body);
-    console.log(req.files)
-	console.log(req.myData)
+// 	// console.log(req.body)
+// 	// console.log(req.files)
 
-	// const fileName = req.files.myData.name
+// 	// //move to local file system
+// 	// const fileName = req.files.myData.name
+// 	// await req.files.myData.mv(`./uploads/${fileName}`)
 
-	// // move to my local directory
-	// const move = await req.files.myData.mv(`./uploads/${fileName}`)
-	// console.log(move)
+// 	// // upload to cloudinary
+// 	// const moved = await cloudinary.uploader.upload(`./uploads/${fileName}`)
+// 	// const uploadFile = await fs.unlink(`./uploads/${fileName}`)
 
-	// // upload the file to cloudinary
-	// const uploadRes = await cloudinary.uploader.upload(`./uploads/${fileName}`)
-	// console.log(uploadRes)
+// 	// console.log(moved, uploadFile)
+// 	// console.log("removed")
 
-	res.json({success: true})
-})
+// 	try {
+		
+// 		const bas64FormattedString = await base64.encode(req.files.myData.data)
+// 		// res.json({
+// 		// 	data: req.files.myData,
+// 		// 	data2: req.files.myData.data,
+// 		// 	string: bas64FormattedString
+// 		// })
+// 		// res.send(bas64FormattedString)
 
-// let connection = null
+// 		// const uploadFile = await cloudinary.uploader.upload(bas64FormattedString)
+// 		const uploadFile = await cloudinary.uploader.upload(`data:${req.files.myData.mimetype};base64,${bas64FormattedString}`)
+// 		// res.json({
+// 		// 	success: true,
+// 		// 	json: uploadFile
+// 		// })
+// 		res.send({"success": true})
+// 	} catch (error) {
+// 		console.log(error)
+		
+// 	}
 
-// async function initDatabaseConnection() {
 
-//     connection = await mysql.createConnection({
-//         host: process.env.DB_HOST,
-//         user: process.env.DB_USER,
-//         password: process.env.DB_PASS,
-//         database: process.env.MYSQL_DB
-//     });
-//     connection.connect(function(err){
-//         if (err){
-//           console.log('error connecting:' + err.stack);
-//         }
-//         console.log('Connection Established with Database Successfully');
-//       });
-//     // connection.connect()
-//     // console.log("Connection Established with Database Successfully")
-//     // return connection
-// }
+// })
 
-// initDatabaseConnection()
+
 
 
 // errors: page not found
@@ -95,6 +95,6 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(process.env.PORT, async () => {
-	console.log("Server Initiated; Listening at 3000")
+	console.log("Server Initiated; Listening at", process.env.PORT)
    // await initDatabaseConnection()
 })
